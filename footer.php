0a60877871bd2b98290d9807cd6558e2382e4fd3
Sylvie <sylvie.veyres@gmail.com>
2017-08-22 15:19:54 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,76 @@
+<footer>
+    <section>
+        <div class="container">
+            <h3>Follow me !</h3>
+            <p>Vuoi ricevere news su altri template? <br>
+            Visita Andrea Galanti.it e vedrai tutte le news riguardanti nuovi Theme!<br>
+            Go to: <a href="#">andreagalanti.it</a></p>
+            <p><a href="#">Flatfy Theme</a> by <a href="#">Andrea Galanti</a> is licensed to the public under</p>
+            <p>the <a href="#">Creative Commons Attribution 3.0 License - NOT COMMERCIAL</a>.</p>
+        </div>
+        <div class="container">
+            <h3>Flatfy Theme</h3>
+            <ul>
+                <li>12 Column Grid Bootstrap</li>
+                <li>Form Contact</li>
+                <li>Drag Gallery</li>
+                <li>Full Responsive</li>
+                <li>Lorem Ipsum</li>
+            </ul>
+            <p>Go to: <a href="#">andreagalanti.it/flatfy</a></p>
+        </div>
+    </section>
+
+</footer>
+<script
+     src="https://code.jquery.com/jquery-3.2.1.min.js"
+      integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+      crossorigin="anonymous"></script>
+
+<script src="<?php echo get_stylesheet_directory_uri().'/static/static/js/OwlCarousel2-2.2.1/dist/owl.carousel.min.js'; ?>"></script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+    if ($(this).parents('.last').length) {
+    }
+    $('.one').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+    $('.two').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+});
+</script>
+</body>
+</html>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,223 @@
+
+    <?php get_header()  ?>
+
+    <body>
+        <header>
+            <div class="">
+                <h1>Flatfy</h1>
+                <p>Clean &amp; minimal Theme</p>
+                <div class="buttons">
+                    <a href="#"><button class="button01" type="button" name="button">Twitter</button></a>
+                    <a href="#"><button class="button02" type="button" name="button">Free Download</button></a>
+                </div>
+            </div>
+
+            <img src="<?php echo get_stylesheet_directory_uri().'/static/img/circle.png'; ?>" alt="down">
+
+        </header>
+        <main>
+            <section>
+                <nav>
+                    <h2>Flatfy</h2>
+                    <ul>
+                        <a href="#"><li>What is ?</li></a>
+                        <a href="#"><li>Use it</li></a>
+                        <a href="#"><li>Screenshot</li></a>
+                        <a href="#"><li>Credits</li></a>
+                        <a href="#"><li>Contact</li></a>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <div class="container title">
+                    <h3>What is ?</h3>
+                    <p>A special thanks to Death.</p>
+                </div>
+                <div class="container whatis">
+                    <article class="">
+                        <img src="<?php echo get_stylesheet_directory_uri().'/static/img/tweet.svg'; ?>" alt="tweets">
+                        <h4>Follow Me</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </article>
+                    <article class="">
+                        <img src="<?php echo get_stylesheet_directory_uri().'/static/img/picture.svg'; ?>" alt="tweets">
+                        <h4>Gallery</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </article>
+                    <article class="">
+                        <img src="<?php echo get_stylesheet_directory_uri().'/static/img/retina.svg'; ?>" alt="tweets">
+                        <h4>Retina</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </article>
+                    <article class="">
+                        <img src="<?php echo get_stylesheet_directory_uri().'/static/img/laptop.svg'; ?>" alt="tweets">
+                        <h4>Responsive</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </article>
+                    <article class="">
+                        <img src="<?php echo get_stylesheet_directory_uri().'/static/img/map.svg'; ?>" alt="tweets">
+                        <h4>Google</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </article>
+                    <article class="">
+                        <img src="<?php echo get_stylesheet_directory_uri().'/static/img/browser.svg'; ?>" alt="tweets">
+                        <h4>Bootstrap</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </article>
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <article class="article">
+                        <div>
+                            <h4>Full Responsive</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                            <button class="buttonv" type="button" name="button">View Details</button>
+                            <button class="buttonb" type="button" name="button">View Website</button>
+                        </div>
+                        <div class="">
+                            <img src="<?php echo get_stylesheet_directory_uri().'/static/img/ipad.png'; ?>" alt="">
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <article class="article">
+                        <div class="owl-carousel one owl-theme">
+                            <a  target="_blank" href="static/img/iphone.png"><img class="cursor" src="static/img/iphone.png" alt=""></a>
+                            <a  target="_blank" href="static/img/iphone.png"><img class="cursor" src="static/img/iphone.png" alt=""></a>
+                            <a  target="_blank" href="static/img/iphone.png"><img class="cursor" src="static/img/iphone.png" alt=""></a>
+                        </div>
+
+
+                        <div>
+                            <h4>Drag Gallery</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                            <button class="buttonv" type="button" name="button">View Details</button>
+                            <button class="buttonb" type="button" name="button">View Website</button>
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <article class="article">
+                        <div >
+                            <h4>Font Awesome &amp; Glyphicon</h4>
+                            <p>A special thanks to Death to the Stock Photo for providing the photographs that you see in this template.</p>
+                            <ul>
+                                <li><i class="fa fa-signal" aria-hidden="true"></i> Reliable and Secure Platform</li>
+                                <li><i class="fa fa-refresh" aria-hidden="true"></i> Everything is perfectly orgainized for future</li>
+                                <li><i class="fa fa-headphones" aria-hidden="true"></i> Attach large file easily</li>
+                            </ul>
+                        </div>
+                        <img src="static/img/doge.png" alt="">
+                    </article>
+                </div>
+            </section>
+            <section>
+                <div class="container title">
+                    <h3>Screen App</h3>
+                    <p>A special thanks to Death.</p>
+                    <div class="owl-carousel two owl-theme">
+                        <div class="item"><img class="cursor" src="static/img/slide/1.png" alt=""></div>
+                        <div class="item"><img class="cursor" src="static/img/slide/2.png" alt=""></div>
+                        <div class="item"><img class="cursor" src="static/img/slide/3.png" alt=""></div>
+                        <div class="item"><img class="cursor" src="static/img/slide/1.png" alt=""></div>
+                        <div class="item"><img class="cursor" src="static/img/slide/2.png" alt=""></div>
+                        <div class="item"><img class="cursor" src="static/img/slide/3.png" alt=""></div>
+                    </div>
+                </div>
+
+
+                <div class="container">
+                    <img class="cursor" src="" alt="">
+                </div>
+            </section>
+            <section class ="update">
+                <div class="container title">
+                    <h3>Get Live Updates</h3>
+                    <p>A special thanks to Death.</p>
+                    <button class="buttonv" type="button" name="button">Subscribe to our Newsletter</button>
+                </div>
+            </section>
+            <section>
+                <div class="container title">
+                    <h3>Credits</h3>
+                    <p>A special thanks to Death.</p>
+                </div>
+                <div class="container credits">
+                    <article class="">
+                        <i class="fa fa-desktop" aria-hidden="true"></i>
+                        <div class="">
+                            <h4>Bootstrap</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                        </div>
+                    </article>
+                    <article class="">
+                        <i class="fa fa-picture-o" aria-hidden="true"></i>
+                        <div class="">
+                            <h4>Owl-Carrousel</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                        </div>
+                    </article>
+                    <article class="">
+                        <i class="fa fa-magic" aria-hidden="true"></i>
+                        <div class="">
+                            <h4>Codrops</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                        </div>
+                    </article>
+                    <article class="">
+                        <i class="fa fa-heart" aria-hidden="true"></i>
+                        <div class="">
+                            <h4>Lorem Ipsum</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section class ="download">
+                <div class="container title">
+                    <h3>Download Free</h3>
+                    <button class="buttonv" type="button" name="button">Pay with a tweet</button>
+                </div>
+            </section>
+            <section>
+                <div class="container title">
+                    <h3>Contact us</h3>
+                    <p>A special thanks to Death.</p>
+                </div>
+                <div class="container contactus">
+                    <form class="" action="index.html" method="post">
+                        <label for="name">Your name</label>
+                        <input type="text" name="name" value="Enter Name">
+                        <label for="email">Your email</label>
+                        <input type="text" name="email" value="Enter Email">
+                        <label for="message">Your message</label>
+                        <textarea name="message" rows="8" cols="80"></textarea>
+                        <button class="buttonv" type="submit" name="button">Submite</button>
+                    </form>
+                    <div class="adress">
+                        <h4>Office Location</h4>
+                        <p>The Pentagon <br>
+                            Washington, DC 20301 <br>
+                            Phone: XXX-XXX-XXXX <br>
+                            Fax: XXX-XXX-YYYY
+                        </p>
+                        <h4>Social</h4>
+                        <ul>
+                            <li><i class="fa fa-facebook-square" aria-hidden="true"></i></li>
+                            <li><i class="fa fa-twitter-square" aria-hidden="true"></i></li>
+                            <li><i class="fa fa-google-plus-square" aria-hidden="true"></i></li>
+                            <li><i class="fa fa-flickr" aria-hidden="true"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <?php get_footer() ?>
